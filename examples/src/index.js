d9bf8c498fdfb812b73d68623b9c570af4774c24
chang20159 <chang20159@foxmail.com>
2017-07-20 14:27:47 +0800

    fix router

--- examples/src/index.js
+++ examples/src/index.js
@@ -20,3 +20,17 @@ new Vue({
     render: h => h(App),
     router
 })
+
+
+//导航钩子,每次导航滚动至顶部,但会首页停留在原位置
+let bodyScrollTop = 0
+router.beforeEach((to, from, next) => {
+    to.path !== '/' && (bodyScrollTop = document.body.scrollTop)
+    next()
+})
+
+router.afterEach(route => {
+    Vue.nextTick(()=>{
+        document.body.scrollTop = route.path === '/' ? bodyScrollTop : 0
+    })
+})
